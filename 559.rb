a251d96546b437c737eea4402c600e63a6553d49
ladan12 <nastialadanivska@gmail.com>
2015-05-26 22:56:53 +0300

    Friendly with rubocop.

--- 559.rb
+++ 559.rb
@@ -1,22 +1,20 @@
-puts "Input n: "
- n = gets.chomp.to_i
-
- def is_prime? n
- 	(2...n).each do |number|
-		if n % number == 0
-			return false
-			break		
-		end
- 	end
- end
-
-res = Array.new
-a = 1
-mersen_number = 1
-while mersen_number < n
- 	res.push(mersen_number) if (is_prime?(a) && is_prime?(mersen_number))
- 	a += 1
- 	mersen_number = 2**a - 1
+def prime?(n)
+  (2...n).each do |number|
+    return false if (n % number == 0)
+    true
+  end
 end
- 	 "#{res.each {|x| puts x}}"
- 	 
+
+def start
+  puts 'Input n: '
+  n = gets.chomp.to_i
+  res = []
+  count = mersen_number = 1
+  while mersen_number < n
+    res.push(mersen_number) if prime?(count) && prime?(mersen_number)
+    count += 1
+    mersen_number = 2**count - 1
+  end
+  res.each { |x| puts x }
+end
+start
