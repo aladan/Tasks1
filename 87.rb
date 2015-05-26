0a60c6d40b234f3b65dccece4cb04a0437c61bf1
ladan12 <nastialadanivska@gmail.com>
2015-05-26 22:52:15 +0300

    Friendly with rubocop.

--- 87.rb
+++ 87.rb
@@ -1,16 +1,23 @@
 # using math logic
-while(true)
-puts "Input n: "
-n = gets.chomp.to_i
-puts "Input m: "
-m = gets.chomp.to_i
-
-result = 0
-	(1..m).each do 
-		result += n % 10
-		n /= 10.0
-	end
-puts "Result = #{result}"
-puts "To exit press 'q', to continue press any key"
-if gets.chomp == 'q' then break end
+require 'pry'
+def calculation(n, m)
+  result = 0
+  (1..m).each do
+    result += n % 10
+    n /= 10
+  end
+  result
 end
+
+def start
+  loop do
+    puts 'Input n: '
+    n = gets.chomp.to_i
+    puts 'Input m: '
+    m = gets.chomp.to_i
+    puts "Result = #{calculation(n, m)}\"
+    To exit press 'q', to continue press any key"
+    gets.chomp == 'q' ? break : true
+  end
+end
+start
